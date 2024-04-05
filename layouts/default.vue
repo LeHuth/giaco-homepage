9e8453bd7fa6c5e5dcfc00f8fee81bc578066674
leo@tower <leonard.huth@googlemail.com>
2024-04-06 01:35:47 +0200

    added video

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,9 @@
 <div>
   <Header/>
   <slot class="z-40 p-10"/>
-<!--  <video class="absolute top-0 left-0 h-full object-cover -z-20 opacity-20" autoplay muted loop id="myVideo">
+  <video class="absolute top-0 left-0 h-full object-cover -z-20 opacity-20" autoplay muted loop id="myVideo">
     <source src="/clip.mp4" type="video/mp4">
-  </video>-->
+  </video>
 </div>
 </template>
 
